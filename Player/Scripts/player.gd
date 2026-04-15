b8eacb6dc05e09e3e007761bb0daf9a82cc6ce0c
Carlos Mareco <carlosmareco2001@gmail.com>
2026-04-14 22:07:37 -0300

    Add: TileMaps, TileSets, Player Camera; Update: Player Movement

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -17,8 +17,12 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	# Definindo a direção em que o player está olhando
-	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
-	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	#direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	#direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	direction = Vector2(
+		Input.get_axis("left", "right"),
+		Input.get_axis("up", "down")
+	).normalized()
 	
 	pass
 
