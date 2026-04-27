557aa42e429e9ddbee7911596e614a64aff8f9c1
Carlos Mareco <carlosmareco2001@gmail.com>
2026-04-27 20:12:51 -0300

    Update: HurtBox (AttackHurtBox now), Player Movement

--- GeneralNodes/HurtBox/hurt_box.gd
+++ GeneralNodes/HurtBox/hurt_box.gd
@@ -6,7 +6,7 @@ func _ready() -> void:
 	area_entered.connect(AreaEntered)
 	pass
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 func AreaEntered(a : Area2D) -> void:
--- Player/Scripts/state_attack.gd
+++ Player/Scripts/state_attack.gd
@@ -13,7 +13,7 @@ var attacking : bool = false
 
 @onready var walk : State = $"../Walk"
 @onready var idle : State = $"../Idle"
-@onready var hurt_box: HurtBox = $"../../Interactions/HurtBox"
+@onready var hurt_box: HurtBox = %AttackHurtBox
 
 
 ## What happens when the player enters this State?
